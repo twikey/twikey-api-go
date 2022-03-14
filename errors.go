f87c4b41ea285687ccf4b0f77e14e00986be9065
Koen Serry <482723+koen-serry@users.noreply.github.com>
2022-03-14 07:12:38 +0100

    Expose TwikeyError elements

--- errors.go
+++ errors.go
@@ -3,26 +3,26 @@ package twikey
 import "net/http"
 
 type TwikeyError struct {
-	status  int
-	code    string
-	message string
-	extra   string
+	Status  int
+	Code    string
+	Message string
+	Extra   string
 }
 
 func (err *TwikeyError) Error() string {
-	return err.message
+	return err.Message
 }
 
 func (err *TwikeyError) IsUserError() bool {
-	return err.status == 400
+	return err.Status == 400
 }
 
 func NewTwikeyError(code string, msg string, extra string) *TwikeyError {
 	return &TwikeyError{
-		status:  400,
-		code:    code,
-		message: msg,
-		extra:   extra,
+		Status:  400,
+		Code:    code,
+		Message: msg,
+		Extra:   extra,
 	}
 }
 
@@ -30,19 +30,19 @@ func NewTwikeyErrorFromResponse(res *http.Response) *TwikeyError {
 	if res.StatusCode == 400 {
 		errcode := res.Header["Apierror"][0]
 		return &TwikeyError{
-			status:  res.StatusCode,
-			code:    errcode,
-			message: errcode,
+			Status:  res.StatusCode,
+			Code:    errcode,
+			Message: errcode,
 		}
 	}
 	return &TwikeyError{
-		status:  res.StatusCode,
-		code:    "system_error",
-		message: res.Status,
+		Status:  res.StatusCode,
+		Code:    "system_error",
+		Message: res.Status,
 	}
 }
 
 var SystemError error = &TwikeyError{
-	status: 500,
-	code:   "system_error",
+	Status: 500,
+	Code:   "system_error",
 }
