c4fca88ad964472e778271dc181637202de0b43f
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2024-03-13 13:49:10 +0100

    Initial changes to Client constructor to make it more end-user friendly

--- twikey_test.go
+++ twikey_test.go
@@ -1,6 +1,7 @@
 package twikey
 
 import (
+	"log"
 	"os"
 	"testing"
 )
@@ -9,6 +10,40 @@ func newTestClient() *Client {
 	c := NewClient(os.Getenv("TWIKEY_API_KEY"))
 	c.BaseURL = getEnv("TWIKEY_URL", "https://api.beta.twikey.com")
 	return c
+}
+
+func TestClientWithBasicFunctionalOptions(t *testing.T) {
+	salt := "salty"
+	baseUrl := "http://localtest.me"
+	userAgent := "someUserAgent"
+	apikey := "123"
+
+	c := NewClient(apikey,
+		WithBaseURL(baseUrl),
+		WithSalt(salt),
+		WithUserAgent(userAgent),
+		WithLogger(log.Default()),
+	)
+
+	if c.APIKey != apikey {
+		t.Fatalf("Expected client ApiKey to be %s but was %s", apikey, c.APIKey)
+	}
+
+	if c.BaseURL != baseUrl {
+		t.Fatalf("Expected client BaseURL to be %s but was %s", baseUrl, c.BaseURL)
+	}
+
+	if c.Salt != salt {
+		t.Fatalf("Expected client Salt to be %s but was %s", salt, c.Salt)
+	}
+
+	if c.UserAgent != userAgent {
+		t.Fatalf("Expected client UserAgent to be %s but was %s", userAgent, c.UserAgent)
+	}
+
+	if c.Debug != log.Default() {
+		t.Fatalf("Expected the default logger from log to be used")
+	}
 }
 
 func TestTwikeyClient_verifyWebhook(t *testing.T) {
